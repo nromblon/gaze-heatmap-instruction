00a80e3b5201bfe6c809445722db9ff7881978ba
nromblon <neilromblon@gmail.com>
2025-07-10 14:07:19 +0900

    Added sidebar component - visual only

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,63 @@
+import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupLabel, SidebarHeader, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar";
+import { SidebarItemObject } from "@/types/sidebar-types";
+import { FileUp, Home, ImagePlus } from "lucide-react";
+
+const instructionMenuItems : SidebarItemObject[] = [
+  {
+    name: "Current Manual",
+    url: "#",
+    icon: Home
+  },
+  {
+    name: "Load Gaze Data",
+    url: "#",
+    icon: FileUp
+  }
+];
+
+const setupMenuItems : SidebarItemObject[] = [
+  {
+    name: "Create Manual",
+    url: "#",
+    icon: ImagePlus
+  },
+];
+
+export default function AppSidebar() {
+  return (
+    <Sidebar side="left" variant="sidebar" className="shadow-md">
+        <SidebarHeader className="bg-white">
+          <h1 className="text-xl mt-2"> Gaze Heatmap </h1>
+        </SidebarHeader>
+      <SidebarContent className="bg-white">
+        <SidebarGroup>
+          <SidebarGroupLabel> Instruction </SidebarGroupLabel>
+          {instructionMenuItems.map((item) => (
+            <SidebarMenuItem key={item.name} style={{ listStyleType: "none" }}>
+              <SidebarMenuButton asChild>
+                <a href={item.url}>
+                  <item.icon />
+                  <span> {item.name} </span>
+                </a>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarGroup>
+        <hr className="border-b border-gray-200 mx-3" />
+        <SidebarGroup>
+          <SidebarGroupLabel> Setup </SidebarGroupLabel>
+          {setupMenuItems.map((item) => (
+            <SidebarMenuItem key={item.name} style={{ listStyleType: "none" }}>
+              <SidebarMenuButton asChild>
+                <a href={item.url}>
+                  <item.icon />
+                  <span> {item.name} </span>
+                </a>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          ))}
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
---
+++ src/types/sidebar-types.tsx
@@ -0,0 +1,7 @@
+import { LucideIcon } from "lucide-react";
+
+export type SidebarItemObject = {
+  name: string;
+  url: string;
+  icon: LucideIcon;
+};
