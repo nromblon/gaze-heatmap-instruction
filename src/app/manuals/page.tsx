f1e67999a287bd605169816493b4198dabf1e416
nromblon <neilromblon@gmail.com>
2025-07-28 16:48:54 +0900

    added saved manuals tab

--- src/app/manuals/page.tsx
+++ src/app/manuals/page.tsx
@@ -1,18 +1,24 @@
 "use client";
 
 import { useAppSelector } from "@/lib/hooks";
+import { cn } from "@/lib/utils";
+import { useState } from "react";
 
 export default function ManualsPage() {
+
+  const [selectedManual, setSelectedManual] = useState<number>(0);
 
   const manuals = useAppSelector((state) => state.manuals.manuals);
 
   return (
     <>
     { manuals.length > 0 ? (
-      <div>
-        {manuals.map((manual) => (
-          <div key={manual.name} className="manual-item">
-            <h2>{manual.name}</h2>
+      <div className="m-5 flex flex-col w-full">
+        {manuals.map((manual, idx) => (
+          <div key={manual.name} className={cn("manual-item w-full mb-4 p-4 border rounded-lg hover:bg-gray-100", 
+            selectedManual === idx ? "bg-gray-100 border-black" : "")}
+            onClick={() => setSelectedManual(idx)}>
+            <h2><span className="font-bold"> Manual Name: </span> {manual.name}</h2>
             <ul>
               {manual.pages.map((page) => (
                 <li key={page.filename}>
