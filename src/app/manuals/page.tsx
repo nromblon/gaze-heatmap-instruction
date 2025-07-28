2172925ca42af76530e4d739a1bd5259ed867d91
nromblon <neilromblon@gmail.com>
2025-07-28 16:53:46 +0900

    added curmanual setting in redux store

--- src/app/manuals/page.tsx
+++ src/app/manuals/page.tsx
@@ -1,14 +1,28 @@
 "use client";
 
-import { useAppSelector } from "@/lib/hooks";
+import { setCurrentManual } from "@/lib/features/manual/manualSlice";
+import { useAppDispatch, useAppSelector } from "@/lib/hooks";
 import { cn } from "@/lib/utils";
 import { useState } from "react";
 
 export default function ManualsPage() {
-
-  const [selectedManual, setSelectedManual] = useState<number>(0);
-
   const manuals = useAppSelector((state) => state.manuals.manuals);
+  const currentManual = useAppSelector((state) => state.manuals.currentManual);
+
+  let curManIdx = manuals.findIndex(manual => manual.name === currentManual?.name);
+  // If currentManual is not set, default to the first manual
+  curManIdx = curManIdx !== -1 ? curManIdx : 0;
+
+  const dispatch = useAppDispatch();
+
+  const [selectedManual, setSelectedManual] = useState<number>(curManIdx);
+
+
+  const handleSelectManual = (idx: number) => {
+    setSelectedManual(idx);
+    // Additional logic can be added here if needed, such as fetching manual details
+    dispatch(setCurrentManual(manuals[idx]));
+  }
 
   return (
     <>
@@ -17,7 +31,7 @@ export default function ManualsPage() {
         {manuals.map((manual, idx) => (
           <div key={manual.name} className={cn("manual-item w-full mb-4 p-4 border rounded-lg hover:bg-gray-100", 
             selectedManual === idx ? "bg-gray-100 border-black" : "")}
-            onClick={() => setSelectedManual(idx)}>
+            onClick={() => handleSelectManual(idx)}>
             <h2><span className="font-bold"> Manual Name: </span> {manual.name}</h2>
             <ul>
               {manual.pages.map((page) => (
